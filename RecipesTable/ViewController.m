b8364098b80a8acee6b26d8c4ef0498a985a9bf2
thodep <thodep@yahoo.com>
2015-04-14 23:04:07 -0400

    Finish Chapter 3

--- RecipesTable/ViewController.m
+++ RecipesTable/ViewController.m
@@ -14,14 +14,50 @@
 
 @implementation ViewController
 
+{
+    NSArray *recipes;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+   //Initialize table data
+    recipes = @[@"Egg Benedict",
+                @"Mushroom Risotto",
+                @"Full Breakfast",
+                @"Hamburger",
+                @"Ham and Egg Sandwich",
+                @"Creme Brelee",
+                @"White Chocolate Donut",
+                @"Starbucks Coffee",
+                @"Vegetable Curry",
+                @"Instant Noodle with Egg",
+                @"Noodle with BBQ Pork",
+                @"Japanese Noodle with Pork",
+                @"Green Tea",
+                @"Thai Shrimp Cake",
+                @"Angry Birds Cake",
+                @"Ham and Cheese Panini"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:
+(NSIndexPath *)indexPath
+{
+    static NSString *cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    
+    cell.imageView.image =[UIImage imageNamed:@"creme_brelee"];
+    return cell;
 }
 
 @end
